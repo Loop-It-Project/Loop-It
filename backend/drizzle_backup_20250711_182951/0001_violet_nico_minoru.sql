971f907c684d6d2fbf8b934a0895d99f76b3ae31
VinJust <vpj97@outlook.de>
2025-07-14 10:09:46 +0200

    Merged main into feature/k8s

--- backend/drizzle_backup_20250711_182951/0001_violet_nico_minoru.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "universes" ADD CONSTRAINT "universes_creatorId_users_id_fk" FOREIGN KEY ("creatorId") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
--- backend/drizzle_backup_20250711_182951/0003_jittery_blur.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "geoTrackingEnabled" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "geoTrackingAccuracy" varchar(20) DEFAULT 'city' NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "autoUpdateLocation" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "showDistanceToOthers" boolean DEFAULT true NOT NULL;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "maxSearchRadius" integer DEFAULT 100 NOT NULL;
